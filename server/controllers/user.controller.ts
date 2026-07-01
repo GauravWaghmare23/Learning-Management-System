446bb7b56d570ae3d9b6acd06166f5b81fe65373
Gaurav Waghmare <gauravwaghmare95032@gmail.com>
2026-07-01 19:38:42 +0530

    refactor(auth): reorganize authentication into controller-service architecture

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,196 +1,24 @@
 require("dotenv").config();
-import { Response, Request, NextFunction } from "express";
-import ErrorHandler from "../utils/ErrorHandler";
-import User, { IUser } from "../models/user.model";
+import { RequestHandler } from "express";
 import { CatchAsyncError } from "../middlewares/catchAsyncErrors";
-import jwt, { Secret } from "jsonwebtoken";
-import ejs from "ejs";
-import path from "path";
-import sendMail from "../utils/sendMail";
+import authService from "../services/auth.service";
 
-// interfaces
 
-interface IActivationToken {
-    token: string;
-    activationCode: string;
+class AuthController{
+
+    public registerUserController:RequestHandler = CatchAsyncError(
+        async(req,res,next) => {
+            const response = await authService.registerUserService(req.body);
+            res.status(201).json(response);
+        }
+    )
+
+    public activateUserController:RequestHandler = CatchAsyncError(
+        async(req,res,next) => {
+            const response = await authService.activateUserService(req.body);
+            res.status(201).json(response);
+        }
+    )
 }
 
-interface IRegisterUser {
-    name: string;
-    email: string;
-    password: string;
-    avatar?: string;
-}
-
-interface IActivationPayload {
-    name: string;
-    email: string;
-    password: string;
-    avatar?: string;
-}
-
-export const createActivationToken = (user: IActivationPayload): IActivationToken => {
-    const activationCode = Math.floor(100000 + Math.random() * 900000).toString();
-    const token = jwt.sign({ user, activationCode }, process.env.ACTIVATION_SECRET_KEY as Secret, {
-        expiresIn: "10m",
-    });
-    return { token, activationCode };
-}
-
-// register User controller
-export const registerUser = CatchAsyncError(
-    async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> => {
-        try {
-            const { name, email, password } = req.body;
-
-            if (!name || !email || !password) {
-                return next(
-                    new ErrorHandler(
-                        "Please provide all required fields",
-                        400
-                    )
-                );
-            }
-
-            const existingUser = await User.findOne({ email });
-
-            if (existingUser) {
-                return next(
-                    new ErrorHandler(
-                        "Email already exists",
-                        409
-                    )
-                );
-            }
-
-            const user: IRegisterUser = {
-                name,
-                email,
-                password,
-            };
-
-            const activationToken = createActivationToken(user);
-
-            const activationCode = activationToken.activationCode;
-
-            const data = { user: { name: user.name }, activationCode };
-
-            const html = await ejs.renderFile(path.join(__dirname, "../mails/activation-mail.ejs"), data);
-
-            try {
-                await sendMail({
-                    email: user.email,
-                    subject: "Activate your account",
-                    template: "activation-mail.ejs",
-                    data
-                });
-
-                res.status(201).json({
-                    success: true,
-                    message: `Registration successful. Please check your ${user.email} to activate your account.`,
-                    activationToken
-                });
-            } catch (error: any) {
-                return next(
-                    new ErrorHandler(
-                        "Failed to send activation email. Please try again.",
-                        500
-                    )
-                );
-            }
-        } catch (error: any) {
-            return next(
-                new ErrorHandler(
-                    "Registration failed. Please try again.",
-                    500
-                )
-            );
-        }
-    }
-);
-
-// activate user
-
-export const activateUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const { activation_token, activation_code } = req.body;
-
-        if (!activation_token || !activation_code) {
-            return next(
-                new ErrorHandler(
-                    "Activation token and code are required",
-                    400
-                )
-            );
-        }
-
-        let decoded: { user: IActivationPayload, activationCode: string };
-
-        try {
-            decoded = jwt.verify(activation_token, process.env.ACTIVATION_SECRET_KEY as Secret) as { user: IActivationPayload, activationCode: string };
-        } catch (error) {
-            if (error instanceof jwt.TokenExpiredError) {
-                return next(
-                    new ErrorHandler(
-                        "Activation token has expired",
-                        401
-                    )
-                );
-            }
-
-            return next(
-                new ErrorHandler(
-                    "Invalid activation token",
-                    401
-                )
-            );
-        }
-
-
-
-        if (decoded.activationCode !== activation_code) {
-            return next(
-                new ErrorHandler(
-                    "Invalid activation code",
-                    400
-                )
-            );
-        }
-
-        const { name, email, password } = decoded.user;
-
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            return next(
-                new ErrorHandler(
-                    "Email already exists",
-                    409
-                )
-            );
-        }
-
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-
-        res.status(201).json({
-            success: true,
-            message: "Account activated and registered successfully",
-        });
-
-    } catch (error: any) {
-        return next(
-            new ErrorHandler(
-                "Failed to activate user. Please try again.",
-                500
-            )
-        );
-    }
-})
+export default new AuthController();
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import { activateUser, registerUser } from "../controllers/user.controller";
+import AuthController from "../controllers/user.controller"
 
 const userRouter = express.Router();
 
-// register user route
-userRouter.post("/registration", registerUser);
-userRouter.post("/activate-user", activateUser);
+userRouter.post("/registration",AuthController.registerUserController);
+userRouter.post("/activate-user",AuthController.activateUserController);
 
 export default userRouter;
